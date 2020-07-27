0fa244c5cb1ca5ea664226b1f5de5cac35520899
Trevis McGill <trevismcgill25@gmail.com>
2020-07-27 18:52:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.uppercase
-    return "NO, NOT SINCE 1938!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
   else
-    return "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
 end
