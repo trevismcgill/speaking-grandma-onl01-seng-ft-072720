c10652bfe72bb3e2d56b3aed94a0aa9e892efbb9
Trevis McGill <trevismcgill25@gmail.com>
2020-07-27 19:05:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,4 +5,5 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!";
+end
